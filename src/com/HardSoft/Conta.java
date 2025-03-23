3ee716d316234084bd9c5a91340a1286d9fce6c2
herculano-jr <herculano-jr@hotmail.com>
2025-03-23 12:13:38 -0300

    Projeto Pronto

--- src/com/HardSoft/Conta.java
+++ src/com/HardSoft/Conta.java
@@ -7,6 +7,35 @@ public class Conta {
 	private String nomeCliente;
 	private double saldo;
 	
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getAgencia() {
+		return agencia;
+	}
+	public void setAgencia(String agencia) {
+		this.agencia = agencia;
+	}
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	public void Mostrar() {
+		System.out.printf("Numero: %d, Agencia: %s, Conta: %s, Saldo: %.2f",this.numero,this.agencia, this.nomeCliente, this.saldo);
+	}
+	
+	
 	
 
 }
--- src/com/HardSoft/ContaTerminal.java
+++ src/com/HardSoft/ContaTerminal.java
@@ -7,8 +7,17 @@ public class ContaTerminal {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+		Conta conta = new Conta();
+		System.out.println("Digite o numero do cliente");
+        conta.setNumero(sc.nextInt());
 		System.out.println("Digite o numero da agencia");
-        String i = sc.next();
+        conta.setAgencia(sc.next());
+        System.out.println("Digite o nome do cliente");
+        conta.setNomeCliente(sc.next());
+        System.out.println("Digite o saldo da conta");
+        conta.setSaldo(sc.nextDouble());
+        conta.Mostrar();
+        
 
 	}
 
